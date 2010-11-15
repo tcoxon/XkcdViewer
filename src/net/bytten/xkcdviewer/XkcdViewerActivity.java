03522c299edde4f3ada87c1d3efb81d853be9cea
Tyler Breisacher <matrixfrog@gmail.com>
2010-11-14 21:11:39 -0800

    better fix for Issue 24 using OnFocusChangeListener instead of Click/TouchListener

--- src/net/bytten/xkcdviewer/XkcdViewerActivity.java
+++ src/net/bytten/xkcdviewer/XkcdViewerActivity.java
@@ -54,13 +54,12 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
+import android.view.View.OnFocusChangeListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -166,17 +165,11 @@ public class XkcdViewerActivity extends Activity {
                 return false;
             }
         }); 
-        comicIdSel.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                comicIdSel.setText("");
-            }
-        });
-        comicIdSel.setOnTouchListener(new OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        comicIdSel.setOnFocusChangeListener(new OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
                     comicIdSel.setText("");
                 }
-                return false;
             }
         });
 
