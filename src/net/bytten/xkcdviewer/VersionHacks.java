51aaf40dcb1b2284a4d72fdba0f00f1f95369a67
Tom Coxon <tom.c.coxon@googlemail.com>
2011-06-16 23:27:02 +0100

    Clean up some whitespace.

--- src/net/bytten/xkcdviewer/VersionHacks.java
+++ src/net/bytten/xkcdviewer/VersionHacks.java
@@ -10,18 +10,18 @@ public class VersionHacks {
     // CREDIT: These two methods are from
     // http://sagistech.blogspot.com/2011/01/buildversionsdkint-on-android-15.html
     //
-    public static int getSdkInt() {  
-        if (Build.VERSION.RELEASE.startsWith("1.5"))  
-            return 3;  
-         
-        return HelperInternal.getSdkIntInternal();  
-    }  
-      
-    private static class HelperInternal {  
-        private static int getSdkIntInternal() {  
-            return Build.VERSION.SDK_INT;                 
-        }  
-    } 
+    public static int getSdkInt() {
+        if (Build.VERSION.RELEASE.startsWith("1.5"))
+            return 3;
+        
+        return HelperInternal.getSdkIntInternal();
+    }
+    
+    private static class HelperInternal {
+        private static int getSdkIntInternal() {
+            return Build.VERSION.SDK_INT;
+        }
+    }
     //
     // !CREDIT:
     //
