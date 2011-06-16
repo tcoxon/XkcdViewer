9b52f7e674c752cb778bdd65642c246f2fe55bfa
Tom Coxon <tom.c.coxon@googlemail.com>
2011-06-16 23:09:21 +0100

    Remove unnecessary import.

--- src/net/bytten/xkcdviewer/XkcdViewerActivity.java
+++ src/net/bytten/xkcdviewer/XkcdViewerActivity.java
@@ -30,7 +30,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
