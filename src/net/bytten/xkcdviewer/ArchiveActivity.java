95b973ff4bbf4296c7fef76cd9a09fa03aafa718
Tom Coxon <tom.c.coxon@googlemail.com>
2011-07-07 20:30:12 +0100

    Remove unused import.

--- src/net/bytten/xkcdviewer/ArchiveActivity.java
+++ src/net/bytten/xkcdviewer/ArchiveActivity.java
@@ -14,7 +14,6 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
